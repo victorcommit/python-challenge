dbec1a018d0fa589fd998056e2988450754835ff
victorcommit <alvinvictor@Alvins-MBP.fios-router.home>
2019-11-10 23:25:25 -0500

    Added comments to the script

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,33 +1,46 @@
+#Import Dependencies
 import os
 import csv
 
+#Specify the file path
 file_infor = os.path.join("..", "PyBank", "budget_data.csv")
 
+#Create empty lists to store data
 total_months = []
 total_profit = []
 change_profit_monthly = []
 
+#Open csv file
 with open(file_infor, newline="", encoding="utf-8") as csvfile:
 
+    #Store csv content as a variable "csvreader"
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    #Skip the header labels
     header = next(csvreader)
 
+    #Iterate through each row in the csv
     for row in csvreader:
 
+        # Append the total months and total profit to their respective list
         total_months.append(row[0])
         total_profit.append(int(row[1]))
 
+    #Iterate through the profits and get the monthly change in profits
     for j in range(len(total_profit)-1):
 
         change_profit_monthly.append(total_profit[j+1]-total_profit[j])
 
+#Calucate the max and min of the monthly profit change
 max_increase_value = max(change_profit_monthly)
 max_decrease_value = min(change_profit_monthly)
 
+
+#Correlate max and min to the proper month using month list and index from max and min, +1 month for next month
 max_increase_month = change_profit_monthly.index(max(change_profit_monthly)) + 1
 max_decrease_month = change_profit_monthly.index(min(change_profit_monthly)) + 1
 
+#Print the Financial Analysis Results
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {len(total_months)}")
@@ -36,11 +49,13 @@ print(f"Average Change: {round(sum(change_profit_monthly)/len(change_profit_mont
 print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
 print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
 
-
+#Create outfile path
 output_file = os.path.join("..", "PyBank", "Financial_Analysis_Summary.txt")
 
+#Open the output file, in "write"
 with open(output_file,"w") as file:
 
+    #Write the results to the new csv fil "Election Results"
     file.write("Financial Analysis")
     file.write("\n")
     file.write("----------------------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,20 +1,33 @@
+#Import Dependencies
 import os 
 import csv
 
+# Specify the file path
 votefile = os.path.join("..", "PyPoll", "election_data.csv")
 
+#Delcare Variables
 total_votes = 0
 khan_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
 
-
+#Open the file, Specify the variable to hold the contents
 with open(votefile,newline="", encoding="utf-8") as votecounts:
+
+    #Store csv content as a variable "csvreader"
     csvreader = csv.reader(votecounts,delimiter=",")
+
+    #Skip the header
     header = next(csvreader)
+
+    #Iterate through each row in the csv
     for row in csvreader:
+
+        #Count the unique Voter IDs and store information as "total_votes"
         total_votes +=1
+
+        #If candidates name is found, count the times appears and store to list 
         if row[2] == "Khan": 
             khan_votes +=1
         elif row[2] == "Correy":
@@ -24,17 +37,21 @@ with open(votefile,newline="", encoding="utf-8") as votecounts:
         elif row[2] == "O'Tooley":
             otooley_votes +=1
 
+#Create a dictionary of unique Voter IDs 
 candidates = ["Khan", "Correy", "Li","O'Tooley"]
 votes = [khan_votes, correy_votes,li_votes,otooley_votes]  
 
+#Combine list of candidates adn votes and return winner with the max
 dict_candidates_and_votes = dict(zip(candidates,votes))
 key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
 
+#Print candidate results in percentage
 khan_percent = (khan_votes/total_votes) *100
 correy_percent = (correy_votes/total_votes) * 100
 li_percent = (li_votes/total_votes)* 100
 otooley_percent = (otooley_votes/total_votes) * 100
 
+#Print the summary of results
 print(f"Election Results")
 print(f"----------------------------")
 print(f"Total Votes: {total_votes}")
@@ -47,10 +64,13 @@ print(f"----------------------------")
 print(f"Winner: {key}")
 print(f"----------------------------")
 
+#Create outfile path
 results = os.path.join("..", "PyPoll", "Election_Results.txt")
 
+#Open the output file, in "write"
 with open(results,"w") as file:
 
+#Write the results to the new csv fil "Election Results"
     file.write(f"Election Results")
     file.write("\n")
     file.write(f"----------------------------")
